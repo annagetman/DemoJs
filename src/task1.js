2280b9ca368d719bc532dbffccbb84c619a456f8
Ann <annaporik12@gmail.com>
2021-08-27 11:52:41 +0300

    Added check to createChessBoard function

--- src/task1.js
+++ src/task1.js
@@ -9,15 +9,20 @@
 let board = ''
 let result = '';
 export const createChessBoard = (length, width, symb) => {
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < width; j++) {
-            //result += (i % 2 === 1) ? ' ' + symb : symb + ' ';
-            result += (i + j) % 2 ? board += symb : board += ' ';
+    if (width || length != 0) {
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < width; j++) {
+                //result += (i % 2 === 1) ? ' ' + symb : symb + ' ';
+                result += (i + j) % 2 ? board += symb : board += ' ';
+            }
+            board += '\n'
         }
-        board += '\n'
+        return board;
     }
-    return board;
+    else {
+        console.log('enter data')
+    }
 }
 
-//createChessBoard(4, 12, '*')
+//createChessBoard(6, 12, '*')
 console.log(board);
